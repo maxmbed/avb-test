d552f30120f979e057c136c9a234427f5c820edb
maxmbed <max.mbeded@gmail.com>
2020-09-30 14:59:54 +0200

    add audio file as source of data

--- source/tsn_talker/tsn_talker.c
+++ source/tsn_talker/tsn_talker.c
@@ -40,6 +40,7 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sndfile.h>
 
 #include "log.h"
 
@@ -50,7 +51,8 @@ static uint8_t macaddr[ETH_ALEN];
 static int priority = -1;
 static size_t size = 1500;
 static uint64_t seq;
-static int delay = -1;
+static int delay = 0;
+static char wav_file_path[PATH_MAX];
 
 static struct argp_option options[] = {
     {"dst-addr", 'd', "MACADDR", 0, "Stream Destination MAC address" },
@@ -58,6 +60,7 @@ static struct argp_option options[] = {
     {"ifname", 'i', "IFNAME", 0, "Network Interface" },
     {"prio", 'p', "NUM", 0, "SO_PRIORITY to be set in socket" },
     {"packet-size", 's', "NUM", 0, "Size of packets to be transmitted" },
+    {"wav file path", 'w', "WAV", 0, "The audio file to send in .wav format"},
     { 0 }
 };
 
@@ -88,12 +91,50 @@ static error_t parser(int key, char *arg, struct argp_state *state)
     case 's':
         size = (size_t)atoi(arg);
         break;
+    case 'w':
+        strncpy(wav_file_path, arg, sizeof(wav_file_path) - 1);
+        break;
     }
 
     return 0;
 }
 
 static struct argp argp = { options, parser };
+
+typedef struct snd_file {
+
+    SF_INFO     info;
+    SNDFILE*    handler;
+    int*        buffer;
+} snd_file_t;
+
+int audio_open_file(snd_file_t* sndfile, char* file_path) {
+
+    sndfile->handler = sf_open(wav_file_path, SFM_READ, &sndfile->info);
+    if (sndfile->handler == NULL) {
+        err("Open audio file\n");
+        return -1;
+    }
+
+    sndfile->buffer = malloc(sndfile->info.frames*sndfile->info.channels);
+    if (sndfile->buffer == NULL) {
+        err("Allocate audio buffer: %s\n", strerror(errno));
+        return -1;
+    }
+    return 0;
+}
+
+long int audio_read_file(snd_file_t* sndfile, sf_count_t num_frames) {
+
+    sf_count_t frame_count;
+
+    frame_count = sf_readf_int(sndfile->handler, sndfile->buffer, num_frames);
+//    if (frame_count != num_frames) {
+//        err("Read audio file\n");
+//        return -1;
+//    }
+    return frame_count;
+}
 
 int main(int argc, char *argv[])
 {
@@ -105,8 +146,13 @@ int main(int argc, char *argv[])
         .sll_protocol = htons(ETH_P_TSN),
         .sll_halen = ETH_ALEN,
     };
+    snd_file_t sndfile;
 
     argp_parse(&argp, argc, argv, 0, NULL, NULL);
+
+    if (audio_open_file(&sndfile, wav_file_path) < 0) {
+        return -1;
+    }
 
     fd = socket(AF_PACKET, SOCK_DGRAM, htons(ETH_P_TSN));
     if (fd < 0) {
@@ -136,28 +182,34 @@ int main(int argc, char *argv[])
 
     data = alloca(size);
     memset(data, MAGIC, size);
-
+    long int counter = 0;
     info("Sending packets...\n");
 
-    while (1) {
+    while (audio_read_file(&sndfile, size) > 0) {
+
         uint64_t *seq_ptr = (uint64_t *) &data[0];
         ssize_t n;
 
         *seq_ptr = seq++;
 
-        n = sendto(fd, data, size, 0, (struct sockaddr *) &sk_addr,
+        info("section %ld/%ld\n", counter, sndfile.info.frames);
+        n = sendto(fd, sndfile.buffer, size, 0, (struct sockaddr *) &sk_addr,
                             sizeof(sk_addr));
         if (n < 0)
             err("Failed to send data: %s\n", strerror(errno));
 
         if (delay > 0)
             usleep((unsigned int)delay);
+
+        counter = counter + size;
     }
 
     close(fd);
+    sf_close(sndfile.handler);
     return 0;
 
 err:
     close(fd);
+    sf_close(sndfile.handler);
     return 1;
 }
